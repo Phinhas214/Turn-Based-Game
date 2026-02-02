e3bfb28d3b2ec94e613592b382020cfc12d74519
Phinhas Asmelash <phinhasasmelash@gmail.com>
2026-02-02 03:04:53 -0800

    added selection visual to active player unit

--- Assets/Scripts/UnitActionSystem.cs
+++ Assets/Scripts/UnitActionSystem.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class UnitActionSystem : MonoBehaviour
 {
+    public static UnitActionSystem Instance { get; private set; }
+
+    public event EventHandler OnSelectedUnitChange;
+
     [SerializeField] private Unit selectedUnit;
     [SerializeField] private LayerMask unitLayerMask;
 
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("There's more than one UnitActionSystem! " + transform + " - " + Instance);
+            Destroy(gameObject);
+            return;
+        } 
+        Instance = this;
+    }
 
     private void Update()
     {
-        if (TryHandleUnitSelection()) return;
+        
         if (Input.GetMouseButtonDown(0))
         {  
+            if (TryHandleUnitSelection()) return;
+            
             selectedUnit.Move(MouseWorld.GetPosition());
         }
     }
@@ -24,12 +40,25 @@ public class UnitActionSystem : MonoBehaviour
         {
             if (raycastHit.transform.TryGetComponent<Unit>(out Unit unit))
             {
-                selectedUnit = unit;
+                SetSelectedUnit(unit);
                 return true;
             }
         }
 
         return false;
+    }
+
+    private void SetSelectedUnit(Unit unit)
+    {   
+        selectedUnit = unit;
+        
+        // if OnSelectedUnitChange doesn't return null -> fire and event
+        OnSelectedUnitChange?.Invoke(this, EventArgs.Empty);
+    }
+
+    public Unit GetSelectedUnit()
+    {
+        return selectedUnit;
     }
     
 }
---
+++ Assets/Scripts/UnitSelectedVisual.cs
@@ -0,0 +1,38 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class UnitSelectedVisual : MonoBehaviour
+{
+    [SerializeField] private Unit unit;
+
+    private MeshRenderer meshRenderer;
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void Start()
+    {
+        UnitActionSystem.Instance.OnSelectedUnitChange += UnitActionSystem_OnSelectedUnitChange;
+
+        UpdateVisual();
+    }
+
+    private void UnitActionSystem_OnSelectedUnitChange(object sender, EventArgs empty)
+    {
+        UpdateVisual();
+    }
+
+    private void UpdateVisual()
+    {
+        if (UnitActionSystem.Instance.GetSelectedUnit() == unit)
+        {
+            meshRenderer.enabled = true;
+        } else
+        {
+            meshRenderer.enabled = false;
+        }
+    }
+}
