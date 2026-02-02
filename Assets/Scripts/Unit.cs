49a833f560dc67cb53dcefaec134b2368650c23c
Phinhas Asmelash <phinhasasmelash@gmail.com>
2026-02-02 01:52:12 -0800

    added prefabs, movement, and player selection

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+
+public class Unit : MonoBehaviour
+{
+    private Vector3 targetPosition;
+
+    private void Awake()
+    {
+        targetPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        float stoppingDistance = 0.1f;
+        if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
+        {
+            Vector3 moveDirection = (targetPosition - transform.position).normalized;
+            float moveSpeed = 4f;
+            transform.position += moveDirection * moveSpeed * Time.deltaTime;
+        }
+        
+        
+    }
+
+    public void Move(Vector3 targetPosition)
+    {
+        this.targetPosition = targetPosition;
+    }
+}
