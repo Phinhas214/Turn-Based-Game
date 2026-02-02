10d2597b776e289374ef6dc7b4aa63a0076f9394
Phinhas Asmelash <phinhasasmelash@gmail.com>
2026-02-02 14:57:03 -0800

    created the grid system and grid object for debugging

---
+++ Assets/Scripts/GridObject.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class GridObject
+{
+    private GridSystem gridSystem;
+    private GridPosition gridPosition;
+
+    public GridObject(GridSystem gridSystem, GridPosition gridPosition)
+    {
+        
+    }
+}
