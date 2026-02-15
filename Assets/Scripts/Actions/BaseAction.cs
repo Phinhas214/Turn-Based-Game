b881b843f35d85816294012400eb14886198b825
Suramya87 <146380955+Suramya87@users.noreply.github.com>
2026-02-14 22:08:14 -0800

    folder fix

---
+++ Assets/Scripts/Actions/BaseAction.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+// "abstract" here won't let us create instances of this class
+// which is what we want
+// we only want to instantitiate classes that inherit base class
+public abstract class BaseAction : MonoBehaviour
+{
+    // classes that extend this class can access these fields
+    protected Unit unit;
+    protected bool isActive;
+    protected Action onActionComplete;
+
+    protected virtual void Awake()
+    {
+        unit = GetComponent<Unit>();
+    }
+
+    // abstract means that we'll be forced to implement this function 
+    // in all the other classes that inherit this BaseAction class
+    public abstract string GetActionName();
+}
